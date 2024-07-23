9fc768306c726f624512be17f4f403a30cf87b4b
Garret Patten <garret.patten@proton.me>
2024-07-22 21:25:28 -0400

    Update dev and ide scripts

--- src/scripts/ide.sh
+++ src/scripts/ide.sh
@@ -2,7 +2,7 @@
 
 workingDirectory=$1
 
-# Update ~/.vimrc
+# Vim configuration
 if [[ ! -f "$HOME/.vimrc" ]]; then
     cp "$workingDirectory/src/dotfiles/vim/.vimrc" "$HOME/.vimrc"
 fi
@@ -14,4 +14,12 @@ if [[ ! -d "$HOME/.config/nvim/" ]]; then
     git clone --depth 1 https://github.com/wbthomason/packer.nvim \
     "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
     sudo dnf install tree-sitter-cli -y
+fi
+
+# VS Code
+if [[ ! -f "/usr/bin/code" ]]; then
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf check-update -y
+    sudo dnf install code -y
 fi
